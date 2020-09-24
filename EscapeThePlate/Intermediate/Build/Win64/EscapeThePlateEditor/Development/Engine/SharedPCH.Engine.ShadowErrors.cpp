274325fcb14b7c80ef0bb00b8a141657bbff4cdd
AdyThomas <u1120386@utah.edu>
2020-09-24 13:03:39 -0600

    Added CPP Files for Onion

---
+++ EscapeThePlate/Intermediate/Build/Win64/EscapeThePlateEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/tommy/OneDrive/Documents/GitHub/EscapeThePlate/EscapeThePlate/Intermediate/Build/Win64/EscapeThePlateEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ EscapeThePlate/Intermediate/Build/Win64/UE4Editor/Inc/EscapeThePlate/Onion.generated.h
@@ -0,0 +1,93 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ESCAPETHEPLATE_Onion_generated_h
+#error "Onion.generated.h already included, missing '#pragma once' in Onion.h"
+#endif
+#define ESCAPETHEPLATE_Onion_generated_h
+
+#define EscapeThePlate_Source_EscapeThePlate_Onion_h_12_SPARSE_DATA
+#define EscapeThePlate_Source_EscapeThePlate_Onion_h_12_RPC_WRAPPERS
+#define EscapeThePlate_Source_EscapeThePlate_Onion_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define EscapeThePlate_Source_EscapeThePlate_Onion_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAOnion(); \
+	friend struct Z_Construct_UClass_AOnion_Statics; \
+public: \
+	DECLARE_CLASS(AOnion, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/EscapeThePlate"), NO_API) \
+	DECLARE_SERIALIZER(AOnion)
+
+
+#define EscapeThePlate_Source_EscapeThePlate_Onion_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAOnion(); \
+	friend struct Z_Construct_UClass_AOnion_Statics; \
+public: \
+	DECLARE_CLASS(AOnion, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/EscapeThePlate"), NO_API) \
+	DECLARE_SERIALIZER(AOnion)
+
+
+#define EscapeThePlate_Source_EscapeThePlate_Onion_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AOnion(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AOnion) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AOnion); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AOnion); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AOnion(AOnion&&); \
+	NO_API AOnion(const AOnion&); \
+public:
+
+
+#define EscapeThePlate_Source_EscapeThePlate_Onion_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AOnion(AOnion&&); \
+	NO_API AOnion(const AOnion&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AOnion); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AOnion); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AOnion)
+
+
+#define EscapeThePlate_Source_EscapeThePlate_Onion_h_12_PRIVATE_PROPERTY_OFFSET
+#define EscapeThePlate_Source_EscapeThePlate_Onion_h_9_PROLOG
+#define EscapeThePlate_Source_EscapeThePlate_Onion_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	EscapeThePlate_Source_EscapeThePlate_Onion_h_12_PRIVATE_PROPERTY_OFFSET \
+	EscapeThePlate_Source_EscapeThePlate_Onion_h_12_SPARSE_DATA \
+	EscapeThePlate_Source_EscapeThePlate_Onion_h_12_RPC_WRAPPERS \
+	EscapeThePlate_Source_EscapeThePlate_Onion_h_12_INCLASS \
+	EscapeThePlate_Source_EscapeThePlate_Onion_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define EscapeThePlate_Source_EscapeThePlate_Onion_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	EscapeThePlate_Source_EscapeThePlate_Onion_h_12_PRIVATE_PROPERTY_OFFSET \
+	EscapeThePlate_Source_EscapeThePlate_Onion_h_12_SPARSE_DATA \
+	EscapeThePlate_Source_EscapeThePlate_Onion_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	EscapeThePlate_Source_EscapeThePlate_Onion_h_12_INCLASS_NO_PURE_DECLS \
+	EscapeThePlate_Source_EscapeThePlate_Onion_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> ESCAPETHEPLATE_API UClass* StaticClass<class AOnion>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID EscapeThePlate_Source_EscapeThePlate_Onion_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
